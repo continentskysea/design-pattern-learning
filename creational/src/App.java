a4dcae6843198c7c322cd03ec94e61bfad411bd4
yoshii <yoshiikempo@gmail.com>
2024-09-30 23:42:41 +0900

    Singletonパターンの学習

---
+++ creational/src/App.java
@@ -0,0 +1,15 @@
+import singleton.Singleton;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("If you see the same value, then singleton was reused (yay!)" + "\n" +
+                "If you see different values, then 2 singletons were created (booo!!)" + "\n\n" +
+                "RESULT:" + "\n");
+
+        // 実行時はFooしかoutputされない
+        Singleton singleton = Singleton.getIncetance("Foo");
+        Singleton anotherSingleton = Singleton.getIncetance("BAR");
+        System.out.println(singleton.value);
+        System.out.println(anotherSingleton.value);
+    }
+}
