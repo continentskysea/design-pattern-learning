e2f9f169de109cd23dc524facad1fdfe30a5923d
yoshii <yoshiikempo@gmail.com>
2024-10-15 02:00:37 +0900

    アダプターパターンの学習

---
+++ structural/src/adapter/adapters/SquarePegAdapter.java
@@ -0,0 +1,23 @@
+package adapter.adapters;
+
+import adapter.round.RoundPeg;
+import adapter.square.SquarePeg;
+
+/**
+ * 変換用アダプター
+ */
+public class SquarePegAdapter extends RoundPeg {
+    private SquarePeg peg;
+
+    public SquarePegAdapter(SquarePeg peg) {
+        this.peg = peg;
+    }
+
+    @Override
+    public double getRadius() {
+        double result;
+        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+        return result;
+    }
+
+}
---
+++ structural/src/adapter/square/SquarePeg.java
@@ -0,0 +1,22 @@
+package adapter.square;
+
+/**
+ * 正方形の杭
+ */
+public class SquarePeg {
+    private double width;
+    
+    public SquarePeg(double width) {
+        this.width = width;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getSquare() {
+        double result;
+        result = Math.pow(this.width, 2);
+        return result;
+    }
+}
