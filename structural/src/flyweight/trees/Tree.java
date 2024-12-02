24df0ebed5a4fea5c660d9d06579514243849aa8
yoshii <yoshiikempo@gmail.com>
2024-12-02 21:12:41 +0900

    flyweightパターンの学習

---
+++ structural/src/flyweight/trees/Tree.java
@@ -0,0 +1,21 @@
+package flyweight.trees;
+
+import java.awt.Graphics;
+/**
+ * それぞれの期の固有状態を含む
+ */
+public class Tree {
+    private int x;
+    private int y;
+    private TreeType type;
+
+    public Tree(int x, int y, TreeType type) {
+        this.x = x;
+        this.y = y;
+        this.type = type;
+    }
+
+    public void draw(Graphics g) {
+        type.draw(g, x, y);
+    }
+}
